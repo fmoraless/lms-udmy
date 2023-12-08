68d5dded9db0ebe2bf769e076e022be5cf5e2109
fmoraless <franticzero@gmail.com>
2023-12-08 20:13:26 -0300

    Add sign-in and dashboard pages

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+const SignInPage = () => {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-700">
+        this is protected page
+      </p>
+    </div>
+  );
+};
+
+export default SignInPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">Hello world</p>
+      <p className="text-3xl font-medium text-sky-700">
+        This is a protected page
+      </p>
       <Button variant="destructive">clik me</Button>
     </div>
   );
