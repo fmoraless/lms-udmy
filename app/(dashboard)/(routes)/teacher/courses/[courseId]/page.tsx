7b45c936bbb0f8e99af2c2b7eb639b7a808a00ce
fmoraless <franticzero@gmail.com>
2023-12-30 15:02:55 -0300

    Add CoursesPage component and update CreatePage component

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,9 @@
+const CoursesPage = ({ params }: { params: { courseId: string } }) => {
+  return (
+    <div>
+      <h1>Course Id: {params.courseId}</h1>
+    </div>
+  );
+};
+
+export default CoursesPage;
